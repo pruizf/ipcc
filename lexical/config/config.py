0d6ce65bca3244f0668fc73fff0b16e96a36db0a
pruiz <pabloruizfabo@gmail.com>
2016-06-03 21:17:46 +0200

    adds some config-checks

--- lexical/config/config.py
+++ lexical/config/config.py
@@ -14,11 +14,15 @@ configdir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 sys.path.append(configdir)
 
+
 # CORPUS
 corpuspath = "/home/pablo/projects/clm/ipcc_norm_wk/CSV_06032016"
-ttgpath = ""
-
+ttgpath = "/home/pablo/projects/clm/ipcc_norm_wk/results_06032016/CSV_06032016_ttg"
+# chosen files as is in ttgpath, but need to find recursively under corpuspath
 chosenfilespath = os.path.join(configdir, "sort_filenames.txt")
+for mypath in (corpuspath, ttgpath, chosenfilespath):
+    assert os.path.exists(mypath)
+
 
 # DATA
 datapath = os.path.join(os.path.join(configdir, os.pardir), "data")
@@ -32,6 +36,9 @@ datakeys = {
     "LKL": {"fn": "likelihood.txt", "tag": None, "runon": "txt"},
     "MDL": {"fn": "modal.txt", "tag": "MD", "runon": "ttg"},
     "PHR": {"fn": "phrases.txt", "tag": None, "runon": "txt"},
-    "PRP": {"fn": "prepositions.txt", "tag": "J.*", "runon": "ttg"}
+    "PRP": {"fn": "prepositions.txt", "tag": "IN", "runon": "ttg"}
 }
 
+for ke in datakeys:
+    assert not (datakeys[ke]["tag"] is not None and
+                datakeys[ke]["runon"] != "ttg")
